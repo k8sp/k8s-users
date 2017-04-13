b7d3df20c479dfd82df4ca00f6d859a039453781
Hui Zhang <zhanghui@unisound.com>
2017-04-13 11:19:18 +0800

    refactor email and add test.

--- main.go
+++ main.go
@@ -6,13 +6,9 @@ import (
 	"fmt"
 	"io"
 	"io/ioutil"
-	"net"
 	"net/http"
 	"os/exec"
-	"strconv"
-	"time"
 
-	"github.com/go-gomail/gomail"
 	"github.com/golang/glog"
 	"github.com/gorilla/mux"
 	"github.com/k8sp/k8s-users/users"
@@ -24,22 +20,6 @@ const (
 	defaultRestartAPIServerScript = "./scripts/restart_apiserver.sh"
 	defaultCertFilesRootPath      = "./testdata/users"
 )
-
-// email context
-const (
-	subject = "k8s key and crt"
-	text    = "Successful: your crt and key for k8s are in attachment!\n"
-)
-
-// adim email info and SMTP address
-type Smtpinfo struct {
-	AdminEmail  string
-	AdminSecrt  string
-	ESMTPServer string
-	subject     string
-	text        string
-	ch          chan *gomail.Message
-}
 
 func main() {
 	addr := flag.String("addr", ":8080", "Listening address")
@@ -57,7 +37,7 @@ func main() {
 		glog.Fatal("Files ca.pem , ca-key.pem , admin email and secrt, smtp server address should be provided.")
 	}
 
-	var smtp = NewSmtpinfo(*smtpsrv, *adminEmail, *adminSecrt)
+	smtp := users.NewSmtpInfo(*smtpsrv, *adminEmail, *adminSecrt)
 
 	// start and run the HTTP server
 	router := mux.NewRouter().StrictSlash(true)
@@ -65,73 +45,10 @@ func main() {
 	glog.Fatal(http.ListenAndServe(*addr, router))
 }
 
-func NewSmtpinfo(e, a, s string) *Smtpinfo {
-	return &Smtpinfo{
-		ESMTPServer: e,
-		AdminEmail:  a,
-		AdminSecrt:  s,
-		subject:     "k8s key and crt",
-		text:        "Successful: your crt and key for k8s are in attachment!\n",
-		ch:          make(chan *gomail.Message),
-	}
-}
-
-func (smtp *Smtpinfo) eSMTPSvcPool() {
-	// SMTP host and port
-	host, port, _ := net.SplitHostPort(smtp.ESMTPServer)
-	portint, err := strconv.Atoi(port)
-	candy.Must(err)
-
-	fmt.Println(smtp)
-	d := gomail.NewDialer(host, portint, smtp.AdminEmail, smtp.AdminSecrt)
-
-	var s gomail.SendCloser
-	open := false
-	for {
-		select {
-		case m, ok := <-smtp.ch:
-			if !ok {
-				return
-			}
-			if !open {
-				// dial to  SMTP
-				s, err = d.Dial()
-				candy.Must(err)
-				open = true
-			}
-			err := gomail.Send(s, m)
-			candy.Must(err)
-
-			// Close the connection to the SMTP server if no email was sent in
-			// the last 30 seconds.
-		case <-time.After(30 * time.Second):
-			if open {
-				err := s.Close()
-				candy.Must(err)
-				open = false
-			}
-		}
-	}
-}
-
-func (s *Smtpinfo) sendEmail(to, crt, key string) {
-	m := gomail.NewMessage()
-	m.SetHeader("From", s.AdminEmail)
-	m.SetHeader("To", to)
-	m.SetHeader("Subject", s.subject)
-	m.SetBody("text/plan", s.text)
-	m.Attach(crt)
-	m.Attach(key)
-
-	fmt.Println("send email")
-	s.ch <- m
-	return
-}
-
-func makeUsersHandler(caKey, caCrt, certFilesRootPath, abacPolicyFile string, smtp *Smtpinfo) http.HandlerFunc {
+func makeUsersHandler(caKey, caCrt, certFilesRootPath, abacPolicyFile string, smtp *users.SmtpInfo) http.HandlerFunc {
 	return makeSafeHandler(func(w http.ResponseWriter, r *http.Request) {
 		// smtp message pool
-		go smtp.eSMTPSvcPool()
+		go smtp.SMTPSvcPool()
 
 		// load ABAC policy file
 		p, err := users.LoadPoliciesfromJSONFile(abacPolicyFile)
@@ -166,15 +83,14 @@ func makeUsersHandler(caKey, caCrt, certFilesRootPath, abacPolicyFile string, sm
 			crtFile, keyFile := users.WriteCertFiles(caCrt, caKey, certFilesRootPath, u.Username)
 
 			// send email
-			smtp.sendEmail(u.Email, crtFile, keyFile)
+			smtp.SendEmail(u.Email, crtFile, keyFile)
 		}
 
 		//save user policy
 		p.DumpJSONFile(abacPolicyFile)
 
 		// restart apiserver to active the new PolicyFile
-		err = shell("docker restart $(docker ps | grep apiserver | awk '{print $1}')")
-		candy.Must(err)
+		_ = shell("docker restart $(docker ps | grep apiserver | awk '{print $1}')")
 		//TODO: implement function by docker client:https://github.com/docker/docker/tree/master/client
 
 	})
