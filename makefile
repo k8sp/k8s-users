e945f398448ca6a8c99958ad5a76e29ff2556349
Hui Zhang <zhanghui@unisound.com>
2017-05-10 15:14:02 +0800

    add comments.

--- makefile
+++ makefile
@@ -1,13 +1,18 @@
 .PHONY:  build 
 
+# generate test data 
 test:
 	./scripts/gen_testdata.sh ./testdata && go test ./...
+# clean all
 clean:
 	go clean && rm -rf ./testdata/*
+# local run
 run:
 	go build && ./scripts/run.sh
+# build k8s-users docker
 build:
 	go build && cp k8s-users build && docker build -t bootstrapper:5000/zhanghui/k8s-users build 
+# local docker run
 docker-run:
 	./scripts/docker-run.sh
 
--- scripts/test.cmd
+++ scripts/test.cmd
@@ -6,11 +6,16 @@
 '
 
 //test to host docker ps from container
-docker run -it --rm -v /var/run/docker.sock:/var/run/docker.sock -v /usr/bin/docker:/usr/bin/docker -v /usr/lib:/usr/lib centos:7.3.1611 /bin/bash
+docker run -it --rm -v /var/run/docker.sock:/var/run/docker.sock \
+                    -v /usr/bin/docker:/usr/bin/docker\
+                    -v /usr/lib:/usr/lib \
+                    centos:7.3.1611 /bin/bash
 
 //target docker to restart
 docker run -d ubuntu:14.04 /bin/bash -c "while ((1)); do sleep 10; done "
 
 //worker
-docker run -it -p 8091:80  -v '/etc/ssl/certs/ca-certificates.crt:/etc/ssl/certs/ca-certificates.crt' -v '/var/run/docker.sock:/var/run/docker.sock' k8s-users 
+docker run -it -p 8091:80  -v '/etc/ssl/certs/ca-certificates.crt:/etc/ssl/certs/ca-certificates.crt'\
+                           -v '/var/run/docker.sock:/var/run/docker.sock'  \
+                           k8s-users 
 
